d0f7798f4dd5d1b7f3ffd7e97b2398324e5bb3b5
Erik Tollerud <erik.tollerud@gmail.com>
2016-05-07 16:54:04 -0700

    switch to using typed memoryview in wrapper

--- find_closest_element.pyx
+++ find_closest_element.pyx
@@ -2,7 +2,7 @@ cimport numpy as c_np
 
 cimport cython_declaration_module
 
-def find_closest_element_wrapper(c_np.ndarray[c_np.float64_t, ndim=1] input_data, key):
+def find_closest_element_wrapper(double[:] input_data, key):
     cdef int result
     result = cython_declaration_module.find_closest_element_in_c(&input_data[0], len(input_data), key)
     return result
