250a8e37cd1e04a8c2fb2520376223496d07b92f
Erik Tollerud <erik.tollerud@gmail.com>
2016-05-07 16:57:44 -0700

    Add docstring

--- find_closest_element.pyx
+++ find_closest_element.pyx
@@ -3,6 +3,22 @@ cimport numpy as c_np
 cimport cython_declaration_module
 
 def find_closest_element_wrapper(double[:] input_data, key):
+    """
+    Find the index into the array ``input_data`` that gives the value closest
+    to ``key``.
+
+    Parameters
+    ----------
+    input_data : 1D array with dtype double
+        The input array to search through.
+    key : float
+        The value to find the closest ``input_data`` element of.
+
+    Returns
+    -------
+    idx : int
+        The index into ``input_data`` of the closest element to ``key``.
+    """
     cdef int result
     result = cython_declaration_module.find_closest_element_in_c(&input_data[0], len(input_data), key)
     return result
